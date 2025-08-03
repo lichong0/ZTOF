74d9feb63bcfa5b76e97e3e7cefc0edf4d7bf66e
Chong Li <133206062+lichong0@users.noreply.github.com>
2025-08-03 23:24:17 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source activate WCA
+# python -u main.py --dataset_name oxford_pet --num_workers 8
+# python -u main_cub.py --dataset_name cub --num_workers 8
+# python -u main_pet.py --dataset_name oxford_pet --num_workers 8
+python -u main_food.py --dataset_name food101 --num_workers 8
+#bash train.sh > pet.log 2>&1
+#bash train.sh > cub.log 2>&1
+#bash train.sh > food.log 2>&1
